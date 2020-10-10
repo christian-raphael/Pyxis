832e320a9455199d8c662226d2b046bab907a04c
gustavo-henrique1 <gustavo.fortes@bandtec.com.br>
2020-10-10 13:45:08 -0300

    tela de login v2 mobile

--- sistema/dashboard/js/AnimationsLogin.js
+++ sistema/dashboard/js/AnimationsLogin.js
@@ -1,3 +1,17 @@
+$('input').focus(function(){
+    $(this).parents('.form-group').addClass('focused');
+});
+
+$('input').blur(function(){
+    var inputValue = $(this).val();
+    if ( inputValue == "" ) {
+        $(this).removeClass('filled');
+        $(this).parents('.form-group').removeClass('focused');  
+    } else {
+        $(this).addClass('filled');
+    }
+})  
+
 
 function colorButton(){
     let login = user.value;
